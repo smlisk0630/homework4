f0a71d88cb1c0f289402dc102249df0cfb34b865
slisk <63676970+smlisk0630@users.noreply.github.com>
2020-12-17 16:39:20 -0500

    added questions to js file

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,59 @@ var quizContainer = document.getElementById("quiz");
 var startBtn = document.getElementById("start");
 var submitBtn = document.getElementById("submit");
 
+var quizQuestions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: {
+            1. "strings",
+            2. "booleans",
+            3. "alerts",
+            4. "numbers"
+        }
+        correctAnswer: "3"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ___.",
+        answers: {
+            1. "quotes",
+            2. "curly brackets",
+            3. "parentheses",
+            4. "square brackets"
+        }
+        correctAnswer: "3"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ___.",
+        answers: {
+            1. "numbers and strings",
+            2. "other arrays",
+            3. "booleans",
+            4. "all of the above"
+        }
+        correctAnswer: "4"
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        answers: {
+            1. "commas",
+            2. "curly brackets",
+            3. "quotes",
+            4. "parentheses"
+        }
+        correctAnswer: "3"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            1. "JavaScript",
+            2. "terminal/bash",
+            3. "for loops",
+            4. "console.log"
+        }
+        correctAnswer: "4"
+    }
+];
+
 quizContainer.addEventListener("click", function(event) {
     if(event.target.matches("button")) {
 
@@ -25,17 +78,13 @@ function startQuiz() {
     
 }
 
-startBtn.addEventListener("click", function(event) {
-
-});
+startBtn.addEventListener("click", startQuiz);
 
 function showResults() {
 
 }
 
-submitBtn.addEventListener("click", function(event) {
-
-});
+submitBtn.addEventListener("click", showResults);
 
 /* see all code in activity 25--one code block excerpted below
 function renderToDos() {
